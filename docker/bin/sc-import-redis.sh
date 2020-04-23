57bda9f95a985e5d75c21939bb9abdcc74f77807
oott123 <git@public.oott123.com>
2020-04-23 13:24:07 +0800

    chore: small changes

--- docker/bin/sc-import-redis.sh
+++ docker/bin/sc-import-redis.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -eo pipefail
 
 php /vagrant/bin/console SaintCoinachJsonCacheCommand
 
 count=$(ls /vagrant/data/gamedocuments/json | wc -l)
 
 for (( i = 0; i <= count; ++i )); do
-  echo "# $i"
+  echo "# $i / $count"
   php /vagrant/bin/console SaintCoinachRedisCommand $i 1 1 -q
 done
