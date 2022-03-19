1b03208af23ec2ae9cb33302685643ec06b79de4
oott123 <git@public.oott123.com>
2022-03-19 23:45:01 +0800

    chore: add q

--- docker/bin/sc-import-redis.sh
+++ docker/bin/sc-import-redis.sh
@@ -4,13 +4,13 @@ set -eo pipefail
 /usr/bin/php /vagrant/bin/console SaintCoinachJsonCacheCommand
 
 
-/usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --content=ENpcBase
+/usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --content=ENpcBase -q
 
 for (( i = 0; i <= 100; ++i )); do
   start_point=$(( $i * 10 ))
 
   echo " --------- Loop: $i / 22 - chunk: $start_point ------------------------------------"
 
-  /usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --start=$start_point --count=10
+  /usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --start=$start_point --count=10 -q
 
 done
