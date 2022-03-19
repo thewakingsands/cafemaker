1465e48936b29232a1dc2966c4a52fc7ed00d089
oott123 <git@public.oott123.com>
2022-03-19 23:13:32 +0800

    fix: import issue

--- docker/bin/sc-import-redis.sh
+++ docker/bin/sc-import-redis.sh
@@ -3,11 +3,14 @@ set -eo pipefail
 
 /usr/bin/php /vagrant/bin/console SaintCoinachJsonCacheCommand
 
-for i in {1..40}
-do
-  start_point=$(( $i * 25 ))
 
-  echo " --------- Loop: $i / 40 - chunk: $start_point ------------------------------------"
+/usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --content=ENpcBase
 
-  /usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --start=$start_point --count=25 --full=1 -q
+for (( i = 0; i <= 100; ++i )); do
+  start_point=$(( $i * 10 ))
+
+  echo " --------- Loop: $i / 22 - chunk: $start_point ------------------------------------"
+
+  /usr/bin/php /vagrant/bin/console SaintCoinachRedisCommand --start=$start_point --count=10
+
 done
